d9bcf402196870ea1a47f6821312e5700267347d
Egorov Evgeniy <egorovzhenya@gmail.com>
2022-09-14 22:54:30 +0400

    feat: configure import/export

---
+++ app.js
@@ -0,0 +1,4 @@
+import defaultData, { Game, readGameSaving as loadGame , writeGameSaving  as saveGame } from './game'
+
+const game = new Game();
+game.start();
---
+++ game.js
@@ -0,0 +1,17 @@
+import defaultData from './domain'
+
+
+class Game {
+    start() {
+      console.log('game started');
+    }
+  }
+
+export default class GameSavingData {
+}
+
+export function readGameSaving() {
+}
+
+export function writeGameSaving() {
+}
